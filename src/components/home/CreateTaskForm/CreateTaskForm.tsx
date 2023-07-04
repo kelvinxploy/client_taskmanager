ae0e9cf6cf961cad5d6a20d4a42e34d73721105f
kelvinxploy <138527936+kelvinxploy@users.noreply.github.com>
2023-07-04 15:52:04 -0400

    :sparkles: Add label select in create task (#7)

--- src/components/home/CreateTaskForm/CreateTaskForm.tsx
+++ src/components/home/CreateTaskForm/CreateTaskForm.tsx
@@ -1,10 +1,17 @@
 import { Listbox, Transition } from '@headlessui/react';
-import { UserCircleIcon } from '@heroicons/react/20/solid';
+import { TagIcon, UserCircleIcon } from '@heroicons/react/20/solid';
 import { FormEvent, Fragment, useState } from 'react';
 
 import UserAvatar from '@/components/common/UserAvatar';
 import Button from '@/components/form/Button';
 import { classNames } from '@/src/utils';
+
+const labels = [
+  { name: 'Tasks', value: 'task' },
+  { name: 'To-Do', value: 'todo' },
+  { name: 'In process', value: 'in-proess' },
+  // More items...
+];
 
 const assignees = [
   { name: 'Unassigned', value: null },
@@ -18,11 +25,12 @@ const CreateTaskForm = (): React.ReactElement => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [assigned, setAssigned] = useState(assignees[0]);
+  const [labelled, setLabelled] = useState(labels[0]);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     // eslint-disable-next-line no-console
-    console.log({ title, description, assigned });
+    console.log({ title, description, assigned, labelled });
   };
 
   return (
@@ -68,7 +76,59 @@ const CreateTaskForm = (): React.ReactElement => {
       </div>
 
       <div className="absolute inset-x-px bottom-0">
-        <div className="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
+        <div className="flex flex-nowrap justify-end space-x-2 px-2 py-2 sm:px-3">
+          <Listbox
+            as="div"
+            value={labelled}
+            onChange={setLabelled}
+            className="flex-shrink-0"
+          >
+            {({ open }): React.ReactElement => (
+              <>
+                <Listbox.Label className="sr-only">Add a label</Listbox.Label>
+                <div className="relative">
+                  <Listbox.Button className="relative inline-flex items-center whitespace-nowrap rounded-full bg-gray-50 px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 sm:px-3">
+                    <TagIcon
+                      className="text-gray-500 h-5 w-5 flex-shrink-0 sm:-ml-1"
+                      aria-hidden="true"
+                    />
+                    <span className="text-gray-900 hidden truncate sm:ml-2 sm:block">
+                      {labelled.value === null ? 'Label' : labelled.name}
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute right-0 z-10 mt-1 max-h-56 w-52 overflow-auto rounded-lg bg-white py-3 text-base shadow ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {labels.map((label) => (
+                        <Listbox.Option
+                          key={label.value}
+                          className={({ active }): string =>
+                            classNames(
+                              active ? 'bg-gray-100' : 'bg-white',
+                              'relative cursor-default select-none px-3 py-2'
+                            )
+                          }
+                          value={label}
+                        >
+                          <div className="flex items-center">
+                            <span className="block truncate font-medium">
+                              {label.name}
+                            </span>
+                          </div>
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
           <Listbox
             as="div"
             value={assigned}
@@ -133,7 +193,8 @@ const CreateTaskForm = (): React.ReactElement => {
               </>
             )}
           </Listbox>
-
+        </div>
+        <div className="flex items-center justify-end space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
           <div className="flex-shrink-0">
             <Button disabled={!title} type="submit">
               Create
