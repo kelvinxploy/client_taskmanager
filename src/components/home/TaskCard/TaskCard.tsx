ad0dd4c3c69d120b31fd018d490bae94816a84ef
Kelvin Lora <kelvin@xploy.net>
2023-07-04 10:38:52 -0400

    :lipstick: Add asignee design in task card

--- src/components/home/TaskCard/TaskCard.tsx
+++ src/components/home/TaskCard/TaskCard.tsx
@@ -8,6 +8,12 @@ const TaskCard = ({ title }: TaskCardProps): React.ReactElement => {
   return (
     <li className="p-2 bg-white shadow-md rounded-md cursor-pointer hover:bg-gray-100">
       <h3 className="text-sm text-gray-600">{title}</h3>
+
+      <div className="mt-3 text-end">
+        <span className="text-xs p-1 text-white rounded-full bg-sky-600">
+          KL
+        </span>
+      </div>
     </li>
   );
 };
