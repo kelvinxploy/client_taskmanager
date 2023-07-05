4fd5eb691cc302074b27057aef81432a146ba2a3
kelvinxploy <138527936+kelvinxploy@users.noreply.github.com>
2023-07-05 17:36:13 -0400

    :sparkles: get tasks from api (#10)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { Task } from '../types/task';
+
 export const classNames = (...classes: string[]): string => {
   return classes.filter(Boolean).join(' ');
 };
@@ -7,4 +9,16 @@ export const getWordsFirstLetterUppercase = (text: string): string => {
     .split(' ')
     .map((word) => word[0].toUpperCase())
     .join('');
+};
+
+export const formatTasksByLabel = (tasks: Task[]): Record<string, Task[]> => {
+  return tasks.reduce((acc, curr) => {
+    if (!acc[curr.label]) {
+      acc[curr.label] = [];
+    }
+
+    acc[curr.label].push(curr);
+
+    return acc;
+  }, {} as Record<string, Task[]>);
 };
